f9fe20455e571c5782ab7f43253eec2c9c9b1fb5
KSilver <nad9596@naver.com>
2021-07-24 22:35:14 +0900

    Feat: 이메일 인증

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import styled from "styled-components";
 import '../style/Signup.css';
 
@@ -10,21 +11,46 @@ const Wrapper = styled.div`
     width: 650px;
 `
 
-const Mypage = () => {
+function Mypage () {
+
+    //axios.get("http://localhost:8080/login")
+        //.then(res => {
+            //console.log(res);
+        //})
+        //.catch(res => console.log(res))
+
+    const email = 'test@ewhain.net';
+
+    const axios = require('axios');
+
+    const emailCertify = () => {
+        axios.post('/CheckMail', {
+            email:email
+          })
+          .then(function (response) {
+            console.log(response);
+            alert("인증번호가 전송되었습니다.");
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
     return (
         <Wrapper>
             <h1 className='title'>마이페이지</h1>
             <hr size='10' color='#49A862'></hr>
             <div className="user_box">이화인 인증</div>
-            <span style={{color: '#228B52'}}>이화인 인증이 완료되었습니다. </span><br/>
+            <span style={{color: '#228B52'}}>이화인 인증이 완료되었습니다. <button onClick={emailCertify}>이화인 인증</button> </span><br/>
             <div className="user_box">아이디</div><br/>
             <div className="user_box">비밀번호</div><br/>
             <div className="user_box">비밀번호 확인</div><br/>
             <div className="user_box">깃허브 아이디</div><br/>
-            <div className="user_box">이메일</div><br/>
+            <div className="user_box">이메일</div> <span id="email"></span><br/>
             <button className="modify">정보수정하기</button>
         </Wrapper>
     )
+
 }
 
 export default Mypage;
